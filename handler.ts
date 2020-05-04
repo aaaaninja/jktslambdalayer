46cadcd403c9665980c78eeede2c50f17141f317
aaaaninja <aaaaninja@aaaa.ninja>
2020-05-04 19:29:45 +0900

    はい (layer作成できるように)

--- handler.ts
+++ handler.ts
@@ -1,7 +1,9 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import axios from 'axios'
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  console.log(axios)
   return {
     statusCode: 200,
     body: JSON.stringify({
